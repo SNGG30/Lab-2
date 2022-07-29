745d78ac6172d0afc668c130ed8dd0c17698fba7
Felipe Garcia <Felipex.Garcia11@Gmail.com>
2022-07-29 13:07:43 -0600

    Lab 2 v1

---
+++ src/lab2p2_felipegarcia_12211249/Lab2P2_FelipeGarcia_12211249.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2p2_felipegarcia_12211249;
+
+/**
+ *
+ * @author felip
+ */
+public class Lab2P2_FelipeGarcia_12211249 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
